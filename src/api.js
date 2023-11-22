d88041f66b6bafcb81f476eee4306ead7bcb8b64
majd dayoub <majd.dayoub04@gmail.com>
2023-11-22 11:35:46 -0500

    Uploading Weather App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "a47dbed391msh003b3eb5998bf61p1658efjsn797c02626b58",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "c5541942cc9fcc234db8717bf93df30f";
+
